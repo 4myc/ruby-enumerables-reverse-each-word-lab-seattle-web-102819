7fd4ec433cd0b5877e34611ca0140c331f85c035
Amy C <missch3n@gmail.com>
2019-10-26 23:45:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,7 @@ def reverse_each_word(sentence1)
   array = sentence1.split("")
   new_string = []
   array.each do |word|
-    new_string.push("#{word.revers)")
+    new_string.push("#{word.reverse}")
+  end
+  
 end
