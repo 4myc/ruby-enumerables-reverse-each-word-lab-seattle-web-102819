8bcede6d22b69f73d0cd4f49f2c6a84e355c64fe
Amy C <missch3n@gmail.com>
2019-10-26 23:56:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-  def reverse_each_word(sentence1)
-  array1= sentence1.split(" ")
-  new_string=[]
-  array1.each do |word|
-    new_string.push("#{word.reverse}")
+def reverse_each_word
+  array = sentence1.split(" ")
+  new_string = []
+  array.each do |word|
+    new_string << word.reverse
   end
-  new_string.join(" ")
+  return new_string.join(" ")
 end
