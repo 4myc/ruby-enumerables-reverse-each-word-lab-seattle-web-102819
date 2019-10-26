c0187dba03fd589d019f7621f9202ea03f186700
Amy C <missch3n@gmail.com>
2019-10-26 23:54:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   array = sentence1.split(" ")
   new_string = []
   array.each do |word|
-    puts new_string << word.reverse
+    new_string << word.reverse
   end
   new_string.join(" ")
 end
